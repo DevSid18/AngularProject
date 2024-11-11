1426eb5c6deb2e8608dd43e715598e0e0c714b58
dhans <dhans@LAPTOP-0AL0T55M>
2024-11-11 23:27:22 +0530

    Api method changes as per new modifications from database

--- StoreManagementApi/StoreManagementApi/Entitiy/CustomerModel.cs
+++ StoreManagementApi/StoreManagementApi/Entitiy/CustomerModel.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace StoreManagementApi.Entity
 {
     public class CustomerModel
@@ -6,10 +8,18 @@ namespace StoreManagementApi.Entity
         public string? firstName { get; set; }
         public string? middleName { get; set; }
         public string? lastName { get; set; }
+        public string? country { get; set; }
+        public string? state { get; set; }
+        public string? district { get; set; }
+        public string? phyAddress { get; set; }
         public string? email { get; set; }
         public string? contact { get; set; }
-        public string? phyAddress { get; set; }
-        public string? action {get;set;}
+        public string? gender { get; set; }
+        public string? password { get; set; }
+        public string? confirmPassword { get; set; }
+        public bool isActive { get; set; }
+        public string? action { get; set; }
         public string? result { get; set; }
+
     }
 }
